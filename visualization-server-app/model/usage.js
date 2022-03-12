5956f811abf1b941a9eb976c97584ce7a2c1eda2
Joshua Peddle <37007232+JoshuaPeddle@users.noreply.github.com>
2022-03-12 12:52:22 -0330

    Add support for building database
    I wrote a small standalone script called build_database.js in /utils/.
    This takes our raw datasets and adds them to mongoDB.
    I kept the name 'datasets' for the db. There are two collections 'usage' and 'response' they correspond to the Usage class and Response class.

--- visualization-server-app/model/usage.js
+++ visualization-server-app/model/usage.js
@@ -1,19 +1,25 @@
 const e = require('express');
 const client = require('../utils/db.js');
 
-async function _get_response_data (){
-    let db = await client.getDb();
-    return await db.collection('datasets');
+/**
+ * Gets the usage collection
+ * @returns The usage collection from MongoDB
+ */
+ async function _get_usage_collection() {
+    let db = await mongo.getDb();
+    return await db.collection('usage');
 };
 
 class Usage {
     constructor(geo, serviceType, ageGroup, income, uom, value) { //Columns B, D, E, F, G, M 
         this.geo = geo //province/region in canada, or Canada as a whole.
-        this.serviceType //"Internet at home", "Smart home devices", etc
+        this.serviceType = serviceType //"Internet at home", "Smart home devices", etc
         this.ageGroup = ageGroup
+        this.income = income 
         this.unit = uom //whether  this is number of people or percentage of people.
         //Arguably above should be translated, for display purposes, into either "%" or ""
         //for this model, everything is a percentage - this unit is included for consistency.
         this.value = value 
     }
 }
+module.exports.Usage = Usage
---
+++ visualization-server-app/utils/build_database.js
@@ -0,0 +1,95 @@
+/*
+ *  This file is meant to be run as a standalone script.
+ *  Used to build mongo database.
+ *  The database name is defined in utils/db.js
+ *  "Opinions on technology" is stored in collection 'response'
+ *  "Uses of Technology" is stored in collection 'usage'
+ * 
+ */
+
+
+const fs = require('fs');
+const mongo = require('./db.js');
+const Response = require('../model/response').Response;
+const Usage = require('../model/usage').Usage;
+
+/**
+ * Gets the response collection
+ * @returns The response collection from MongoDB
+ */
+async function _get_response_collection() {
+    let db = await mongo.getDb();
+    return await db.collection('response');
+};
+
+
+/**
+ * Gets the usage collection
+ * @returns The usage collection from MongoDB
+ */
+async function _get_usage_collection() {
+    let db = await mongo.getDb();
+    return await db.collection('usage');
+};
+
+/**
+ * Creates two collections in mongo database 'datasets'
+ *  "Opinions on technology" is stored in collection 'response'
+ *  "Uses of Technology" is stored in collection 'usage'
+ *  @returns 'success' if everything worked
+ */
+async function buildDB() {
+    // Connect to our DB
+    await mongo.connectToDB();
+
+    // Get our response collection
+    let responseCollection = await _get_response_collection();
+    // Read 'datasets\Use of Technology by Age.csv' 
+    let responseData = fs.readFileSync("../datasets/Opinions on Technology.tsv", 'utf8');
+    // Empty list to store the Response objects
+    let responseArray = []
+    //Split the data by lines and construct Response objects
+    // Slice off header data
+    responseData.split('\n').slice(start = 1).forEach(async (el) => {
+        let rawData = el.split("\t");
+        // Construct response from split data
+        let newResponse = new Response(rawData[1], rawData[3], rawData[4], rawData[5], rawData[6], rawData[7], rawData[8], rawData[14])
+        // Add to array
+        responseArray.push(newResponse)
+       
+    })
+    // Add all Responses at once to collection 'response'
+    await responseCollection.insertMany(responseArray)
+
+
+    // Repeat the above process for 'Use of Technology by Age.csv'
+
+    let usageCollection = await _get_usage_collection();
+    let usageData = fs.readFileSync("../datasets/Use of Technology by Age.tsv", 'utf8');
+    let usageArray = []
+ 
+    usageData.split('\n').slice(start = 1).forEach(async (el) => {
+        let rawData = el.split("\t");
+        //console.log(rawData)
+        let newUsage = new Usage(rawData[1], rawData[3], rawData[4], rawData[5], rawData[6], rawData[12])
+        // Add to array
+        usageArray.push(newUsage)
+       
+    })
+    await usageCollection.insertMany(usageArray)
+
+    return "success"
+}
+
+
+
+
+buildDB()
+    .then((res) => {
+        console.log("DB creation successful")
+        console.log(res)
+        mongo.closeDBConnection();
+    })
+    .catch((err) => {
+        console.log(err);
+    });
