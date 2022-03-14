c294e25732ce860d7282345cae2f7fadcb0f5105
ethantmc <77448566+ethantmc@users.noreply.github.com>
2022-03-14 15:44:40 -0230

    getResponses changed to getUsages

--- visualization-server-app/controller/usages.js
+++ visualization-server-app/controller/usages.js
@@ -2,6 +2,6 @@ const Usage = require('../model/usage').Usage;
 const fs = require('fs');
 const v = require('../utils/validate-fields');
 
-module.exports.getResponses = async (req,res) =>{
+module.exports.getUsages = async (req,res) =>{
 
 }
