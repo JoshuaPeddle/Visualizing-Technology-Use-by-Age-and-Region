e58785e3fa0bb6eb6ded51561d28b468d852cfbf
ethantmc <77448566+ethantmc@users.noreply.github.com>
2022-04-05 17:29:40 -0230

    comment added

--- visualization-server-app/view/frontend.js
+++ visualization-server-app/view/frontend.js
@@ -15,6 +15,7 @@ $(function () {
     $("#requestSearch").click(function(e){
         //functionality wise, to handle multiple selections, we pass arrays containing the search terms into usageSearch and responsesSearch. 
         //Later in this, we remove any key with only empty arrays.
+        //If there are multiple selections, do we need to slice out additional searches? Will we need multiple calls to the gets?
         //Note that geos, while shared, depends on the disabling of geographical regions not shared by Usages and Responses so they are not in this input.
         //geos must also now be translated, since we use Value for geojson on them. 
         let [geos,sexes,serviceTypes,ageGroupsUsages,ageGroupsResponses,incomes,questions,responses] = [[],[],[],[],[],[],[],[]] //this assigns all eight variables their own individual empty arrays on a single line.
