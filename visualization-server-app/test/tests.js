50e79af830dcaf40fa644a2a5237f17928d7be00
Joshua Peddle <37007232+JoshuaPeddle@users.noreply.github.com>
2022-04-08 12:02:33 -0230

    Fix failed response test. Validator removal changed error message.

--- visualization-server-app/test/tests.js
+++ visualization-server-app/test/tests.js
@@ -77,7 +77,7 @@ describe('Visualizing-Technology - Tests with Mocha', function () {
                 let res = await instance.get('/responses', { params: sampleSearch });
                 // Should get a message back from the server 'no responses'
                 // This could be validated better
-                assert.strictEqual(res.data, 'invalid query'); // Added error message to differentiate between empty query: 'no responses' and an invalid query: 'invalid query'
+                assert.strictEqual(res.data, 'no responses'); // Added error message to differentiate between empty query: 'no responses' and an invalid query: 'invalid query'
             });
             it('Comprehensive Filter Test - all filters return correctly.', async function () {
                 // construct a sample search with invalid search params
